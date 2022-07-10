08dded80885e5c9b208f9dbbe3d809a3540eac7b
Jussi Kujala <jussi.kujala@iki.fi>
2022-07-10 17:36:58 +0300

    Add simple unit test: run with pytest

---
+++ tests/test_tgc_utils.py
@@ -0,0 +1,36 @@
+import hashlib
+import pytest
+from PIL import Image
+from tgc_utils.convert_card_images import convert_card_img
+from tgc_utils.convert_card_images import create_overlay_img
+
+
+@pytest.fixture
+def config():
+  config = {
+    "margin": 55,
+    "size": (825, 1125),
+  }
+  return config
+
+
+@pytest.fixture
+def overlay_img(config):
+  return create_overlay_img(config)
+
+
+@pytest.fixture
+def img(config):
+  img = Image.new("RGB", config["size"], color="white")
+  return img
+
+
+def test_convert_card_img(img, overlay_img, config):
+  converted_img = convert_card_img(img, overlay_img, config)
+  img_bytes = converted_img.tobytes()
+  m = hashlib.md5()
+  m.update(img_bytes)
+  img_hash = m.hexdigest()
+  assert img_hash == "38a8e7f55a5668accef2517ca9a2e50e"
+
+
